b9b00f8751feb6186f5075a2bdb72680451f7070
Lagg333673034 <lagg333673034@yandex.ru>
2023-10-28 17:36:39 +0400

    5553

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('../routes/index');
+const config = require('config');
+const router = require('./routes/index');
 const fileUpload = require('express-fileupload');
 const path = require('path');
-const serverless= require('serverless-http');
-const config = require('config');
 const PORT = config.get('serverPort');
 const app = express();
-
-
-app.get('/123', (req, res) => {
-    res.send('<h1>Home page</h1>');
-});
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -25,7 +19,18 @@ app.use(function(req, res, next) {
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static(path.resolve(__dirname, 'static')));
-app.use('/',router);
-mongoose.connect(config.get('dbURL'));
+//app.use(express.static(path.join(__dirname, '../client/build')));
 
-module.exports.handler = serverless(app);
+app.use('/', router);
+
+const start = async () => {
+    try{
+        await mongoose.connect(config.get('dbURL'));
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`)
+        });
+    }catch(e){
+        console.log(e);
+    }
+};
+start();
