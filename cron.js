98ab001b7590a2cb5cb131c7b154e455d2fadad7
Lagg333673034 <lagg333673034@yandex.ru>
2023-11-15 19:16:33 +0400

    3334

--- cron.js
+++ cron.js
@@ -1,8 +1,39 @@
 const cron = require('cron');
+const https = require('https');
 
-const job = new cron.CronJob('*/14 * * * *', function(){console.log('not sleep')});
 //'* */1 * * * *'  //every seconds
 //'*/1 * * * *'  //every minutes
 //'*/14 * * * *'  //every minutes
+const job = new cron.CronJob('*/1 * * * *', function(){
+    console.log('not sleep');
+
+    /*https
+        .get("https://www.google.ru",(res)=>{
+            if(res.statusCode === 200) {
+                console.log('ok :)');
+            }else{
+                console.error('failed :(');
+            }
+        })
+        .on('error',(err)=>{
+            console.error('error');
+        });*/
+
+    const url = "https://www.google.ru";
+    //const url = "https://ru.wikipedia.org/wiki/Список_серий_аниме_One_Piece";
+    https
+        .get(url,(res)=>{
+            let data = '';
+            res.on('data',(chank) => {
+                data += chank;
+            });
+            res.on('end',() => {
+                console.log(data);
+            });
+        })
+        .on('error',(err)=>{
+            console.log('error: ' + err.message);
+        });
+});
 
 module.exports = job;
