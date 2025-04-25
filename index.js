5aec78c11a493ac08b82071fcdf392b3e9a47fde
Lagg333673034 <144306091+Lagg333673034@users.noreply.github.com>
2025-04-25 23:52:03 +0400

    3333

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const router = require('./routes/index');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 
-//const config = require('config');
-//const PORT = config.get('serverPort');
+
 const SERVER_PORT = process.env.SERVER_PORT;
 const DB_URL = process.env.DB_URL;
 const CLIENT_HOST = process.env.CLIENT_HOST;
@@ -20,11 +19,6 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
 
 
 const cors = require('cors');
-/*app.use(cors({
-    credentials: true,
-    //origin: "http://localhost:3000"
-    origin: `http://${HOST}:3000`
-}));*/
 
 
 const corsOptions = {
@@ -36,21 +30,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-/*app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', 'true');
-    //res.header('Access-Control-Allow-Origin', 'http://'+CLIENT_HOST);
-    res.header('Access-Control-Allow-Origin', 'http://silver-cucurucho-d41282.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});*/
-
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(express.static(path.resolve(__dirname, 'static')));
-//app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', router);
 
@@ -58,10 +42,6 @@ app.use(errorMiddleware);
 
 const start = async () => {
     try{
-        /*await mongoose.connect(config.get('dbURL'));
-        app.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`)
-        });*/
         await mongoose.connect(DB_URL);
         app.listen(SERVER_PORT, () => {
             console.log(`Server started on port ${SERVER_PORT}`)
